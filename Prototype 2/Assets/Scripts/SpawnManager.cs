2a5418b1abd22dcb59814d9f5f0a9d6c947ed5c8
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-01-14 14:39:43 -0800

    2.2.3

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
     void Start()
     {
         
@@ -13,6 +15,9 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
