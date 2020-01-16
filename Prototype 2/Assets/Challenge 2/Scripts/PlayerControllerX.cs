21ca7af07a2cfb28f556c1ccc143ccc463a5f288
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-01-16 14:46:58 -0800

    Challenge 2.7

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -7,11 +7,14 @@ public class PlayerControllerX : MonoBehaviour
     public GameObject dogPrefab;
 
     // Update is called once per frame
+    private float spawnRate = 0.5f;
+    private float nextSpawn = 0.0f;
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawn)
         {
+            nextSpawn = Time.time + spawnRate;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
